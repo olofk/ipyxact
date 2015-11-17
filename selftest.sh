0dfb3179f2de2878cb080ac3a27f35f665038c80
Olof Kindgren <olof.kindgren@gmail.com>
2015-11-17 09:55:38 +0100

    Use local ipyxact module for selftest.sh

--- selftest.sh
+++ selftest.sh
@@ -1,13 +1,13 @@
 echo Running gen_c_header
-python gen_c_header.py generic_example.xml > out.h
+PYTHONPATH=ipyxact python gen_c_header.py generic_example.xml > out.h
 echo Running gen_markdown
-python gen_markdown.py generic_example.xml > out.md
+PYTHONPATH=ipyxact python gen_markdown.py generic_example.xml > out.md
 echo Running ipxactwriter
-python ipxactwriter.py
+PYTHONPATH=ipyxact python ipxactwriter.py
 echo Running print_businterfaces
-python print_businterfaces.py generic_example.xml > bus.txt
+PYTHONPATH=ipyxact python print_businterfaces.py generic_example.xml > bus.txt
 echo Running print_filesets
-python print_filesets.py generic_example.xml > files.txt
+PYTHONPATH=ipyxact python print_filesets.py generic_example.xml > files.txt
 echo Running write_xml
-python write_xml.py generic_example.xml
+PYTHONPATH=ipyxact python write_xml.py generic_example.xml
 
